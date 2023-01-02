2a5a725c690e4a2519cc5b056572a811838e6382
Bruno Bulnes <bulnes.bruno@gmail.com>
2023-01-02 12:57:14 -0300

    feat: setting theme provider for components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,15 @@
 import Button from "../src/components/Button/Button";
+import ThemeProvider from "../src/theme/ThemeProvider";
 
 export default function HomeScreen() {
   return (
-    <div>
-      <h1>Home Screen</h1>
-      <Button>
-        My button
-      </Button>
-    </div>
+    <ThemeProvider>
+      <div>
+        <h1>Home Screen</h1>
+        <Button>
+          My button
+        </Button>
+      </div>
+    </ThemeProvider>
   )
 }
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,19 +1,43 @@
 import React from "react";
+import styled from "styled-components";
 
 interface ButtonProps {
+  variant?: 'primary' | 'accent';
   disabled?: boolean;
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
   children: React.ReactNode;
 }
+
+const StyledButton = styled.button<ButtonProps>`
+  cursor: pointer;
+  border: 0;
+  padding: 8px 14px;
+  transition: 0.3s;
+
+  ${({ theme, variant }) => ({
+    backgroundColor: theme.colors[variant].main,
+    color: theme.colors[variant].text,
+
+    ':hover': {
+      backgroundColor: theme.colors[variant].light,
+    },
+
+    ':focus': {
+      backgroundColor: theme.colors[variant].dark,
+    },
+  })}
+`
+
 export default function Button({ children, ...props }: ButtonProps) {
   return (
-    <button type="button" {...props}>
+    <StyledButton type="button" {...props}>
       {children}
-    </button>
+    </StyledButton>
   )
 }
 
 Button.defaultProps = {
+  variant: 'primary',
   disabled: false,
   onClick: () => {}
 }
